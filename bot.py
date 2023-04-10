dd7bc6206920e3c0a66faf122fbced58d703d86e
NoahHallows <73092265+NoahHallows@users.noreply.github.com>
2023-04-10 11:23:45 +1000

    Here be code

--- bot.py
+++ bot.py
@@ -13,11 +13,17 @@ async def on_message(message):
     if message.author == client.user:
         return
     number_up_to = message.content
-    number_up_to = int(number_up_to)
-    number_up_to = number_up_to + 1
-    number_up_to = str(number_up_to)
-    keyboard.write(number_up_to)
-    keyboard.press_and_release("enter")
+    print(number_up_to)
+    print(len(number_up_to))
+    try:
+        number_up_to = int(number_up_to)
+        number_up_to = number_up_to + 1
+        number_up_to = str(number_up_to)
+        keyboard.write(number_up_to)
+        keyboard.press_and_release("enter")
+    except:
+        await message.channel.send("Error")
+    print("done")
     #await message.channel.send(number_up_to)
  
-client.run('Access token')
+client.run('MTA5NDc2NDk3NzM4ODI2MTQxOA.GA51K_.nnYqd8aoSLaA3Y9fnjVbGb2KY2C6kOi1hEEEmw')
